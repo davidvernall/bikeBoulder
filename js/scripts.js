6d4a271dcaf8f5b1e01ac80c3df684fc6f8b0d99
David Vernall <davidvernall@hotmail.com>
2021-03-18 16:02:03 +0000

    updated cards, and masthead

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,3 @@
-// (function ($) {
-//     "use strict"; // Start of use strict
-
-//     // Smooth scrolling using jQuery easing
-//     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-//         if (
-//             location.pathname.replace(/^\//, "") ==
-//                 this.pathname.replace(/^\//, "") &&
-//             location.hostname == this.hostname
-//         ) {
-//             var target = $(this.hash);
-//             target = target.length
-//                 ? target
-//                 : $("[name=" + this.hash.slice(1) + "]");
-//             if (target.length) {
-//                 $("html, body").animate(
-//                     {
-//                         scrollTop: target.offset().top - 70,
-//                     },
-//                     1000,
-//                     "easeInOutExpo"
-//                 );
-//                 return false;
-//             }
-//         }
-//     });
-
-//     // Closes responsive menu when a scroll trigger link is clicked
-//     $(".js-scroll-trigger").click(function () {
-//         $(".navbar-collapse").collapse("hide");
-//     });
-
-//     // Activate scrollspy to add active class to navbar items on scroll
-//     $("body").scrollspy({
-//         target: "#mainNav",
-//         offset: 100,
-//     });
-
-//     // Collapse Navbar
-//     var navbarCollapse = function () {
-//         if ($("#mainNav").offset().top > 100) {
-//             $("#mainNav").addClass("navbar-shrink");
-//         } else {
-//             $("#mainNav").removeClass("navbar-shrink");
-//         }
-//     };
-//     // Collapse now if page is not at top
-//     navbarCollapse();
-//     // Collapse the navbar when page is scrolled
-//     $(window).scroll(navbarCollapse);
-// })(jQuery); // End of use strict
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
@@ -64,22 +13,10 @@ function ride() {
     }
 }
 
-// WEATHER APP
-var weather = require('weather-js');
-
-// Options:
-// search:     location name or zipcode
-// degreeType: F or C
-
-weather.find({search: 'Boulder, CO', degreeType: 'F'}, function(err, result) {
-  if(err) console.log(err);
-
-  console.log(JSON.stringify(result, null, 2));
-});
 
 // Search bar
 function rideFunction() {
-    var input, filter, cards, cardContainer, h5, title, i;
+    var input, filter, cards, cardContainer, title, i;
     input = document.getElementById("rideFilter");
     filter = input.value.toUpperCase();
     cardContainer = document.getElementById("rideItems");
@@ -93,3 +30,12 @@ function rideFunction() {
         }
     }
 }
+
+// Card shadow effects
+$( ".card" ).hover(
+    function() {
+      $(this).addClass('shadow').css('cursor', 'pointer'); 
+    }, function() {
+      $(this).removeClass('shadow');
+    }
+  );
